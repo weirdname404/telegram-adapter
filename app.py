a0524922a359906b2abc093f8018380112777f23
Alex Lebedev <lebedev.alex.dev@gmail.com>
2019-11-17 23:35:21 +0300

    [ref] minor refactoring

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List, Type, Dict
+from typing import List, Type, Dict, Callable
 from requests import Session
 import os
 import sys
@@ -13,14 +13,18 @@ logger.setLevel(logging.DEBUG)
 
 
 class Webhook(BaseModel):
+    """
+    Telegram message text will be sent to a given url
+    in a JSON format: {json_key: text}
+    """
     url: str
-    json_key: str = os.environ.get('WEBHOOK_POST_KEY', 'question')
+    json_key: str
 
 
 class TGAdapter(BaseModel):
     '''
-    Listens to TG app updates and sends them to the webhook url,
-    webhook's response adapter returns to the TG app
+    Listens to Telergam app updates and sends them to a given webhook.
+    Webhook's response is sent back to the Telegram app.
     '''
     app_token: str
     webhook: Webhook
@@ -31,18 +35,15 @@ class TGAdapter(BaseModel):
     def _get_updates(self, last_update_id: int, update_url: str, timeout: int=300) -> Dict:
         # incr the offset by 1 to get updates after the last one
         resp = self.tg_session.post(
-                update_url, data={'timeout': timeout, 'offset': last_update_id + 1}
-                )
+                update_url, data={'timeout': timeout, 'offset': last_update_id + 1})
+
         if not resp:
             return []
 
         if resp.status_code != 200:
-            logger.info(f"Status code is not 200. {resp.text}")
+            logger.debug(f"Status code is not 200. {resp.text}")
 
         return resp.json()['result']
-
-    def _send_reply(self, api_url: str, data: Dict) -> None:
-        self.tg_session.post(api_url, data=data)
 
     @staticmethod
     def _get_values_to_text(d: Dict) -> str:
@@ -54,30 +55,35 @@ class TGAdapter(BaseModel):
 
         return s
 
-    def _send_webhook_request(self, text: str) -> Dict:
+    def _get_webhook_request_func(self) -> Callable:
         url = self.webhook.url
         json_key = self.webhook.json_key
+        session = self.webhook_session
 
-        resp = self.webhook_session.post(
-                url, data={json_key: text}
-            )
+        def send_webhook_request(text: str) -> Dict:
+            res = session.post(url, data={json_key: text})
+            if res.status_code != 200:
+                logger.debug(f"Something wrong with a webhook. {res.text}")
+                return {}
+            return res.json()
 
-        return resp.json()
+        return send_webhook_request
 
     def run(self):
-        api_update_url = f"{self.tg_api_url}{self.app_token}/getUpdates"
-        api_send_url = f"{self.tg_api_url}{self.app_token}/sendMessage"
-
+        get_update_url = f"{self.tg_api_url}{self.app_token}/getUpdates"
+        send_msg_url = f"{self.tg_api_url}{self.app_token}/sendMessage"
+        send_webhook_request = self._get_webhook_request_func()
         update_id = 0
+
         logger.info('Listening to updates...')
         while True:
-            for update in self._get_updates(update_id, api_update_url):
+            for update in self._get_updates(update_id, get_update_url):
                 update_id = update['update_id']
                 msg = update['message']
-                webhook_resp = self._send_webhook_request(msg['text'])
+                webhook_resp = send_webhook_request(msg['text'])
                 text = self._get_values_to_text(webhook_resp)
                 self.tg_session.post(
-                    api_send_url, 
+                    send_msg_url, 
                     data={'chat_id': msg['chat']['id'], 'text': text}
                 )
 
@@ -90,7 +96,7 @@ def main():
         logger.info("Exiting...")
         sys.exit()
 
-    webhook = Webhook(url=webhook_url)
+    webhook = Webhook(url=webhook_url, json_key='question')
     tg_adapter = TGAdapter(app_token=app_token, webhook=webhook)
     tg_adapter.run()
 
